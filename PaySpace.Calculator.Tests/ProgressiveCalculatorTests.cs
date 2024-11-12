e6806e6a3706b97e2855d44c706352232d79b4ad
Ockert van Heerden <ockertvh@payspace.com>
2024-11-12 15:53:33 +0200

    Initial commit

---
+++ PaySpace.Calculator.Tests/ProgressiveCalculatorTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+
+namespace PaySpace.Calculator.Tests
+{
+    [TestFixture]
+    internal sealed class ProgressiveCalculatorTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [TestCase(-1, 0)]
+        [TestCase(50, 5)]
+        [TestCase(8350, 835)]
+        [TestCase(8351, 835.15)]
+        [TestCase(33951, 4675.25)]
+        [TestCase(82251, 16750.28)]
+        [TestCase(171550, 41734)]
+        [TestCase(999999, 327683.15)]
+        public async Task Calculate_Should_Return_Expected_Tax(decimal income, decimal expectedTax)
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
